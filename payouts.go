8b756a7ec0a44a8ab31eb94a3bbd87a2f31c12d4
suhrt <suhrt2@gmail.com>
2023-04-13 17:44:23 +0530

    added additonal fields to payout

--- payouts.go
+++ payouts.go
@@ -3,8 +3,13 @@ package structs
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Payouts struct {
-	ID     primitive.ObjectID `bson:"_id,omitempty"`
-	StatId string             `json:"stat_id" bson:"stat_id"`
-	Amount int32             `json:"amount" bson:"amount"`
-	Status string             `json:"status" bson:"status"`
+	ID               primitive.ObjectID  `bson:"_id,omitempty"`
+	PayoutId         string              `json:"payout_id" bson:"payout_id"`
+	StatId           string              `json:"stat_id" bson:"stat_id"`
+	Amount           int                 `json:"amount" bson:"amount"`
+	SettlementAmount int                 `json:"settlement_amount" bson:"settlement_amount"`
+	RxIds            []string            `json:"rx_ids" bson:"rx_ids"`
+	TimeStamp        primitive.Timestamp `json:"time_stamp" bson:"time_stamp"`
+	Status           string              `json:"status" bson:"status"`
 }
+
