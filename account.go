edd0b1e655500786fc925ee0bd210de3a7cb97b9
suhrt <suhrt2@gmail.com>
2023-12-15 11:52:15 +0530

    Formatting

--- account.go
+++ account.go
@@ -1,11 +1,12 @@
 package structs
 
 type Account struct {
-	AccountNumber     string `json:"account_number" bson:"account_number"`
-	Verified          bool   `json:"verified" bson:"verified"`
+	AccountNumber      string `json:"account_number" bson:"account_number"`
+	Verified           bool   `json:"verified" bson:"verified"`
 	VerificationStatus string `json:"verification_status" bson:"verification_status"`
-	AccountName       string `json:"account_name" bson:"account_name"`
-	ISFC              string `json:"isfc" bson:"isfc"`
-	FundAccount       string `json:"fund_account" bson:"fund_account"`
-	RazorPayContactId string `json:"razorpay_contact_id" bson:"razorpay_contact_id"`
+	AccountName        string `json:"account_name" bson:"account_name"`
+	ISFC               string `json:"isfc" bson:"isfc"`
+	FundAccount        string `json:"fund_account" bson:"fund_account"`
+	RazorPayContactId  string `json:"razorpay_contact_id" bson:"razorpay_contact_id"`
+	Vpa                string `json:"vpa" bson:"vpa"`
 }
