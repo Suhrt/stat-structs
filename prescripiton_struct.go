57fc45100d5e11eca34ba472f06edd88a4eb8370
suhrt <suhrt2@gmail.com>
2023-12-15 11:52:15 +0530

    Formatting

--- prescripiton_struct.go
+++ prescripiton_struct.go
@@ -5,8 +5,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Prescription struct {
 	ID               primitive.ObjectID `bson:"_id,omitempty"`
 	PrescriptionId   string             `json:"prescription_id" bson:"prescription_id"`
-	Doctor           *Doctor             `json:"doctor,omitempty" bson:"doctor,omitempty"`
-	Patient          *Patient            `json:"patient,omitempty" bson:"patient,omitempty"`
+	Doctor           *Doctor            `json:"doctor,omitempty" bson:"doctor,omitempty"`
+	Patient          *Patient           `json:"patient,omitempty" bson:"patient,omitempty"`
 	DoctorId         string             `json:"doctor_id" bson:"doctor_id"`
 	PatientId        string             `json:"patient_id" bson:"patient_id"`
 	Date             string             `json:"date" bson:"date"`
@@ -17,12 +17,12 @@ type Prescription struct {
 	Drugs            []Drug             `json:"drugs" bson:"drugs"`
 	Investigations   []Investigation    `json:"investigations" bson:"investigations"`
 	Advice           string             `json:"advice" bson:"advice"`
-	Signature        *DisplaySignature   `json:"signature,omitempty" bson:"signature,omitempty"`
+	Signature        *DisplaySignature  `json:"signature,omitempty" bson:"signature,omitempty"`
 	Password         string             `json:"password" bson:"password"`
 	Notes            string             `json:"notes" bson:"notes"`
 	Location         Location           `json:"location" bson:"location"`
 	Charges          []Charges          `json:"charges" bson:"charges"`
-	Order            *Order              `json:"order" bson:"order"`
+	Order            *Order             `json:"order" bson:"order"`
 	DigitalSignature DigitalSignature   `json:"digital_signature" bson:"digital_signature"`
 	HtWt             HtWt               `json:"htw" bson:"htw"`
 	IsOld            bool               `json:"is_old" bson:"is_old"`
