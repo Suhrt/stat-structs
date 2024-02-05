46f5b795c701bf61e14cfc5e288a7f60ae3b8e25
suhrt <suhrt2@gmail.com>
2024-02-05 14:51:53 +0530

    ADD: pt identification marks

--- certificate.go
+++ certificate.go
@@ -3,19 +3,18 @@ package structs
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Certificate struct {
-	ID                      primitive.ObjectID `bson:"_id,omitempty"`
-	CertifcateId            string             `json:"certificate_id" bson:"certifcate_id"`
-	DoctorId                string             `json:"doctor_id" bson:"doctor_id"`
-	PatientId               string             `json:"patient_id" bson:"patient_id"`
-	Password                string             `json:"password" bson:"password"`
-	Doctor                  *Doctor            `json:"doctor,omitempty" bson:"doctor,omitempty"`
-	Patient                 *Patient           `json:"patient,omitempty" bson:"patient,omitempty"`
-	DigitalSignature        DigitalSignature   `json:"digital_signature" bson:"digital_signature"`
-	Charges                 []Charges          `json:"charges" bson:"charges"`
-	Order                   *Order             `json:"order" bson:"order"`
-	TemplateFields          []TemplateField    `json:"template_fields" bson:"template_fields"`
-	Title                   string             `json:"title" bson:"title"`
-	Type                    string             `json:"type" bson:"type"`
-	IncludePatientSignature bool               `json:"include_patient_signature" bson:"include_patient_signature"`
-	IncludePatientAddress   bool               `json:"include_patient_address" bson:"include_address"`
+	ID                       primitive.ObjectID `bson:"_id,omitempty"`
+	CertifcateId             string             `json:"certificate_id" bson:"certifcate_id"`
+	DoctorId                 string             `json:"doctor_id" bson:"doctor_id"`
+	PatientId                string             `json:"patient_id" bson:"patient_id"`
+	Password                 string             `json:"password" bson:"password"`
+	DigitalSignature         DigitalSignature   `json:"digital_signature" bson:"digital_signature"`
+	Charges                  []Charges          `json:"charges" bson:"charges"`
+	Order                    *Order             `json:"order" bson:"order"`
+	TemplateFields           []TemplateField    `json:"template_fields" bson:"template_fields"`
+	Title                    string             `json:"title" bson:"title"`
+	Type                     string             `json:"type" bson:"type"`
+	IncludePatientSignature  bool               `json:"include_patient_signature" bson:"include_patient_signature"`
+	IncludePatientAddress    bool               `json:"include_patient_address" bson:"include_address"`
+	IncludePatientIdentifier string             `json:"include_patient_identifier" bson:"include_patient_identifier"`
 }
--- patient_struct.go
+++ patient_struct.go
@@ -3,26 +3,27 @@ package structs
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Patient struct {
-	ID              primitive.ObjectID  `bson:"_id,omitempty"`
-	PatientId       string              `json:"patient_id" bson:"patient_id"`
-	DoctorAccess    []string            `json:"doctor_access" bson:"doctor_access"`
-	Name            string              `json:"name" bson:"name"`
-	Age             string              `json:"age" bson:"age"`
-	Sex             string              `json:"sex" bson:"sex"`
-	Address         string              `json:"address" bson:"address"`
-	Allergies       string              `json:"allergies" bson:"allergies"`
-	Comorbidities   []string            `json:"comorbidities" bson:"comorbidities"`
-	PastSurgeries   string              `json:"pastSurgeries" bson:"pastSurgeries"`
-	Other           string              `json:"other" bson:"other"`
-	HtWtRecords     []HtWt              `json:"ht_wt_records" bson:"ht_wt_records"`
-	CountryCode     string              `json:"countryCode" bson:"countryCode"`
-	Note            string              `json:"note" bson:"note"`
-	PhoneNumber     string              `json:"phone_number" bson:"phone_number"`
-	Email           string              `json:"email" bson:"email"`
-	Dependent       int32               `json:"dependent" bson:"dependent"`
-	LastUpdated     primitive.Timestamp `json:"last_updated" bson:"last_updated"`
-	Diagnosis       string              `json:"diagnosis" bson:"diagnosis"`
-	ProfileComplete bool                `json:"profile_complete" bson:"profile_complete"`
+	ID                  primitive.ObjectID  `bson:"_id,omitempty"`
+	PatientId           string              `json:"patient_id" bson:"patient_id"`
+	DoctorAccess        []string            `json:"doctor_access" bson:"doctor_access"`
+	Name                string              `json:"name" bson:"name"`
+	Age                 string              `json:"age" bson:"age"`
+	Sex                 string              `json:"sex" bson:"sex"`
+	Address             string              `json:"address" bson:"address"`
+	Allergies           string              `json:"allergies" bson:"allergies"`
+	Comorbidities       []string            `json:"comorbidities" bson:"comorbidities"`
+	PastSurgeries       string              `json:"pastSurgeries" bson:"pastSurgeries"`
+	Other               string              `json:"other" bson:"other"`
+	HtWtRecords         []HtWt              `json:"ht_wt_records" bson:"ht_wt_records"`
+	CountryCode         string              `json:"countryCode" bson:"countryCode"`
+	Note                string              `json:"note" bson:"note"`
+	PhoneNumber         string              `json:"phone_number" bson:"phone_number"`
+	Email               string              `json:"email" bson:"email"`
+	Dependent           int32               `json:"dependent" bson:"dependent"`
+	LastUpdated         primitive.Timestamp `json:"last_updated" bson:"last_updated"`
+	Diagnosis           string              `json:"diagnosis" bson:"diagnosis"`
+	ProfileComplete     bool                `json:"profile_complete" bson:"profile_complete"`
+	IdentificationMarks []string            `json:"identification_marks" bson:"identification_marks"`
 }
 
 type HtWt struct {
