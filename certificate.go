09c94cc014e17cfcd211ac7e0a674bbce7022b41
suhrt <suhrt2@gmail.com>
2024-02-07 15:17:37 +0530

    Certificate

--- certificate.go
+++ certificate.go
@@ -3,18 +3,18 @@ package structs
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Certificate struct {
-	ID                       primitive.ObjectID `bson:"_id,omitempty"`
-	CertifcateId             string             `json:"certificate_id" bson:"certifcate_id"`
-	DoctorId                 string             `json:"doctor_id" bson:"doctor_id"`
-	PatientId                string             `json:"patient_id" bson:"patient_id"`
-	Password                 string             `json:"password" bson:"password"`
-	DigitalSignature         DigitalSignature   `json:"digital_signature" bson:"digital_signature"`
-	Charges                  []Charges          `json:"charges" bson:"charges"`
-	Order                    *Order             `json:"order" bson:"order"`
-	TemplateFields           []TemplateField    `json:"template_fields" bson:"template_fields"`
-	Title                    string             `json:"title" bson:"title"`
-	Type                     string             `json:"type" bson:"type"`
-	IncludePatientSignature  bool               `json:"include_patient_signature" bson:"include_patient_signature"`
-	IncludePatientAddress    bool               `json:"include_patient_address" bson:"include_address"`
-	IncludePatientIdentifier bool               `json:"include_patient_identifier" bson:"include_patient_identifier"`
+	ID                      primitive.ObjectID `bson:"_id,omitempty"`
+	CertifcateId            string             `json:"certificate_id" bson:"certifcate_id"`
+	DoctorId                string             `json:"doctor_id" bson:"doctor_id"`
+	PatientId               string             `json:"patient_id" bson:"patient_id"`
+	Password                string             `json:"password" bson:"password"`
+	Title                   string             `json:"title" bson:"title"`
+	Type                    string             `json:"type" bson:"type"`
+	TemplateFields          []TemplateField    `json:"template_fields" bson:"template_fields"`
+	IncludePatientSignature bool               `json:"include_patient_signature" bson:"include_patient_signature"`
+	PatientSignature        string             `json:"patient_signature" bson:"patient_signature"`
+	PatientAddress          string             `json:"patient_address" bson:"patient_address"`
+	PatientIdentifiers      []string           `json:"patient_identifiers" bson:"patient_identifiers"`
+	Charges                 []Charges          `json:"charges" bson:"charges"`
+	Order                   *Order             `json:"order" bson:"order"`
 }
