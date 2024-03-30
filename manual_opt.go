8052dfdbbdcfd93050cee20ce2ea4a916349b0cd
suhrt <suhrt2@gmail.com>
2024-03-30 22:37:33 +0530

    ADD: Email login, manual otp

--- manual_opt.go
+++ manual_opt.go
@@ -2,11 +2,9 @@ package structs
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type Login struct {
+type ManualOtp struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty"`
 	PhoneNumber string             `json:"phone_number" bson:"phone_number,omitempty"`
 	Email       string             `json:"email" bson:"email,omitempty"`
-	Password    string             `json:"password" bson:"password,omitempty"`
-	StatId      string             `json:"stat_id" bson:"stat_id,omitempty"`
-	OtpKey      string             `json:"otp_key" bson:"otp_key,omitempty"`
+	OTP         string             `json:"otp" bson:"otp,omitempty"`
 }
